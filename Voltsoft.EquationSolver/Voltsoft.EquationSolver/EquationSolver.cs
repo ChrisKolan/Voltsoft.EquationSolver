009e1ca61975a4831a8f2c877f1571ef9b488b08
ChrisKolan <chris.kolann@gmail.com>
2020-10-26 22:46:51 +0100

    Added equation solver

---
+++ Voltsoft.EquationSolver/Voltsoft.EquationSolver/EquationSolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Voltsoft.EquationSolver
+{
+    public class EquationSolver
+    {
+        private readonly double _CoefficientA;
+        private readonly double _CoefficientB;
+        private readonly double _CoefficientC;
+
+        public EquationSolver(double coefficientA, double coefficientB, double coefficientC)
+        {
+            _CoefficientA = coefficientA;
+            _CoefficientB = coefficientB;
+            _CoefficientC = coefficientC;
+        }
+
+        public QuadraticEquationSolver.Solution Solve()
+        {
+            QuadraticEquationSolver.Solution result = null;
+
+            if (_CoefficientA !=0)
+            {
+                result = new QuadraticEquationSolver.Solver(_CoefficientA, _CoefficientB, _CoefficientC).FindQuadraticRoots();
+            }
+
+            return result;
+        }
+    }
+}
